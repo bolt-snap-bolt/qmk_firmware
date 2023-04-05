476d6cb86bdd51b3c7c50c88cbfef7916c4eadd0
Harley Sims <harleysims@gmail.com>
2023-04-04 22:55:54 -0700

    - Add Alt+Tab encoder scrolling - Switch to using tap dance to perform git functions - Add WSL helper tap dance

--- keyboards/keychron/v1/ansi_encoder/keymaps/bolt-snap-bolt/keymap.c
+++ keyboards/keychron/v1/ansi_encoder/keymaps/bolt-snap-bolt/keymap.c
@@ -17,7 +17,7 @@
 #include QMK_KEYBOARD_H
 
 /* bsb keymap */
-#define BSB_VERSION "v1.0.5"
+#define BSB_VERSION "v1.0.7"
 
 // clang-format off
 enum layers
@@ -26,7 +26,8 @@ enum layers
     BASE_LAYER_WORK,
     LAYER_FN,
     LAYER_GIT,
-    LAYER_VSCODE
+    LAYER_VSCODE,
+    LAYER_ENCODER_ALTTAB
 };
 
 // layer shorthands for keymap use
@@ -35,6 +36,7 @@ enum layers
 #define L_FN    LAYER_FN
 #define L_GIT   LAYER_GIT
 #define L_VSC   LAYER_VSCODE
+#define L_ALTB  LAYER_ENCODER_ALTTAB
 
 // layer mask for lighting use
 #define LIGHTING_LAYERS ((layer_state_t) \
@@ -66,19 +68,189 @@ enum layers
 #define CONTINUE (true)     // continue processing this event
 #define FINISHED (false)    // no further processing needed
 
-// define some macro keycodes
+/* Keycodes */
 enum custom_keycodes {
     // see quantum_keycodes.h and keycodes.h
-    FWVER = SAFE_RANGE, 
-    GIT_ST, // git status
-    GIT_DV, // git switch develop
-    GIT_UP, // git update (fetch/pull/subup)
+    FWVER = SAFE_RANGE,
     VS_ST, // vsc debug step
     VS_SI, // vsc debug step in
     VS_SO, // vsc debug step out
     LGVW, // "longview"
 };
 
+/****************** TAP DANCE STUFF ******************/
+
+enum {
+    WSL_H, // wsl helper
+    GT_ST, // git status
+    GT_SW, // git switch
+    ENCDR, // encoder dual-use layer switch
+};
+
+void td_wsl_helper(tap_dance_state_t *state, void *user_data)
+{
+    (void)user_data;
+
+    if (IS_LAYER_ON(LAYER_GIT))
+    {
+        switch (state->count)
+        {
+        case 1:
+            SEND_STRING("cd ~/");
+            break;
+        case 2:
+            SEND_STRING("code .");
+            tap_code(KC_ENTER);
+            break;
+        case 3:
+            SEND_STRING("explorer.exe .");
+            tap_code(KC_ENTER);
+            break;
+        default:
+            break;
+        }
+
+    }
+    reset_tap_dance(state);
+}
+
+void td_git_status(tap_dance_state_t *state, void *user_data)
+{
+    (void)user_data;
+
+    if (IS_LAYER_ON(LAYER_GIT))
+    {
+        SEND_STRING("git ");
+        switch (state->count)
+        {
+        default:
+        case 1:
+            SEND_STRING("status");
+            break;
+        case 2:
+            SEND_STRING("fetch");
+            break;
+        case 3:
+            SEND_STRING("pull");
+            break;
+        case 4:
+            SEND_STRING("fetch && git pull && git submodule update");
+            break;
+        }
+        tap_code(KC_ENTER);
+    }
+    reset_tap_dance(state);
+}
+
+void td_git_switch(tap_dance_state_t *state, void *user_data)
+{
+    (void)user_data;
+
+    if (IS_LAYER_ON(LAYER_GIT))
+    {
+        SEND_STRING("git switch ");
+        switch (state->count)
+        {
+        default:
+        case 1:
+            break;
+        case 2:
+            SEND_STRING("develop");
+            break;
+        case 3:
+            SEND_STRING("feature/");
+            break;
+        }
+    }
+    reset_tap_dance(state);
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [WSL_H] = ACTION_TAP_DANCE_FN(td_wsl_helper),
+    [GT_ST] = ACTION_TAP_DANCE_FN(td_git_status),
+    [GT_SW] = ACTION_TAP_DANCE_FN(td_git_switch),
+};
+
+/****************** END TAP DANCE STUFF ******************/
+
+/****************** ALT TAB ENCODER STUFF ****************/
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+
+bool alt_tab_encoder(uint8_t index, bool clockwise)
+{
+    if (clockwise)
+    {
+        if (!is_alt_tab_active)
+        {
+            is_alt_tab_active = true;
+            register_code(KC_LALT);
+        }
+        alt_tab_timer = timer_read();
+        tap_code16(KC_TAB);
+    }
+    else
+    {
+        if (!is_alt_tab_active)
+        {
+            is_alt_tab_active = true;
+            register_code(KC_LALT);
+        }
+        alt_tab_timer = timer_read();
+        tap_code16(S(KC_TAB));
+    }
+    return false;
+}
+
+/* The encoder_update_user is a function.
+ * It'll be called by QMK every time you turn the encoder.
+ *
+ * The index parameter tells you which encoder was turned. If you only have
+ * one encoder, the index will always be zero.
+ *
+ * The clockwise parameter tells you the direction of the encoder. It'll be
+ * true when you turned the encoder clockwise, and false otherwise.
+ */
+bool encoder_update_user(uint8_t index, bool clockwise)
+{
+    bool return_val = false;
+
+    if (index == 0)
+    {
+        if (IS_LAYER_ON(LAYER_ENCODER_ALTTAB))
+        {
+            return_val = alt_tab_encoder(index, clockwise);
+        }
+        else
+        {
+            // fall back to volume control
+            if (clockwise)
+            {
+                tap_code16(KC_VOLU);
+            }
+            else
+            {
+                tap_code16(KC_VOLD);
+            }
+        }
+    }
+
+    return return_val;
+}
+
+void matrix_scan_user(void)
+{
+    if (is_alt_tab_active)
+    {
+        if (timer_elapsed(alt_tab_timer) > 1250)
+        {
+            unregister_code(KC_LALT);
+            is_alt_tab_active = false;
+        }
+    }
+}
+
+/****************** END ALT TAB ENCODER STUFF ****************/
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_LAYER_HOME] = LAYOUT_ansi_82(
@@ -99,15 +271,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
 
     [LAYER_FN] = LAYOUT_ansi_82(
-        QK_MAKE,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            RGB_TOG,
+        QK_MAKE,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            TG(L_ALTB),
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            LGVW,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            KC_PGDN,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            KC_PGUP,
         _______,            _______,  _______,  _______,  FWVER,    _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  TG(L_VSC),
         _______,  _______,  _______,                                _______,                                _______,  _______,    _______,TG(L_GIT),  _______,  _______),
 
     [LAYER_GIT] = LAYOUT_ansi_82(
-        _______,  _______,  GIT_ST,     GIT_DV,     GIT_UP,     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,TD(WSL_H),TD(GT_ST),TD(GT_SW),  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
@@ -116,6 +288,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_VSCODE] = LAYOUT_ansi_82(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  VS_SO,    VS_ST,    VS_SI,    _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
+
+    // Empty except for encoder map
+    [LAYER_ENCODER_ALTTAB] = LAYOUT_ansi_82(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
@@ -137,11 +318,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [BASE_LAYER_HOME] = { ENCODER_CCW_CW(KC_VOLD,       KC_VOLU)},
-    [BASE_LAYER_WORK] = { ENCODER_CCW_CW(KC_MS_WH_UP,   KC_MS_WH_DOWN)},
-    [LAYER_FN]        = { ENCODER_CCW_CW(KC_VOLD,       KC_VOLU)},
-    [LAYER_GIT]       = { ENCODER_CCW_CW(_______, _______)},
-    [LAYER_VSCODE]    = { ENCODER_CCW_CW(_______, _______)}
+    [BASE_LAYER_HOME]       = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [BASE_LAYER_WORK]       = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
+    [LAYER_FN]              = { ENCODER_CCW_CW(_______, _______)},
+    [LAYER_GIT]             = { ENCODER_CCW_CW(_______, _______)},
+    [LAYER_VSCODE]          = { ENCODER_CCW_CW(_______, _______)},
+    [LAYER_ENCODER_ALTTAB]  = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
 };
 #endif // ENCODER_MAP_ENABLE
 
@@ -161,46 +343,85 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         // global customs
         switch (keycode)
         {
-        /* git commands */
-        case GIT_ST:
-            SEND_STRING("git status -s");
-            tap_code(KC_ENTER);
-            goto exit;
-        case GIT_DV:
-            SEND_STRING("git switch develop");
-            goto exit;
-        case GIT_UP:
-            SEND_STRING("git fetch && git pull && git submodule update");
-            goto exit;
-        /* vsc commands */
-        case VS_ST:
-            tap_code(KC_F10);
-            goto exit;
-        case VS_SI:
-            tap_code(KC_F11);
-            goto exit;
-        case VS_SO:
-            add_mods(MOD_MASK_SHIFT);
-            tap_code(KC_F11);
-            del_mods(MOD_MASK_SHIFT);
-            goto exit;
-        /* misc */
         case LGVW:
             SEND_STRING("longview");
-            goto exit;
+            break;
         case FWVER:
             SEND_STRING(BSB_VERSION);
-            goto exit;
+            break;
         default:
             break;
+        }
+
+        if (IS_LAYER_ON(LAYER_VSCODE))
+        {
+            switch (keycode)
+            {
+            case VS_ST:
+                tap_code(KC_F10);
+                break;
+            case VS_SI:
+                tap_code(KC_F11);
+                break;
+            case VS_SO:
+                add_mods(MOD_MASK_SHIFT);
+                tap_code(KC_F11);
+                del_mods(MOD_MASK_SHIFT);
+                break;
+            default:
+                break;
+            }
         }
     }
 
     return_val = CONTINUE;
-    
-exit:
+
+//exit:
     return return_val;
 };
+
+
+
+// /* LEADER STUFF */
+// void leader_start_user(void) {
+//     // Do something when the leader key is pressed
+// }
+
+// void leader_end_user(void) {
+//     if (leader_sequence_one_key(KC_C))
+//     {
+//         SEND_STRING("code ." KC_ENTER);
+//     }
+//     else if (leader_sequence_one_key(KC_E))
+//     {
+//         SEND_STRING("explorer.exe ." KC_ENTER);
+//     }
+//     else if (leader_sequence_one_key(KC_L))
+//     {
+//         SEND_STRING("longview");
+//     }
+//     // git stuff
+//     else if (leader_sequence_two_keys(KC_G, KC_F))
+//     {
+//         SEND_STRING("git fetch" KC_ENTER);
+//     }
+//     else if (leader_sequence_two_keys(KC_G, KC_s))
+//     {
+//         SEND_STRING("git status" KC_ENTER);
+//     }
+//     else if (leader_sequence_three_keys(KC_D, KC_D, KC_S))
+//     {
+//         // Leader, d, d, s => Types the below string
+//         SEND_STRING("https://start.duckduckgo.com\n");
+//     }
+//     else if (leader_sequence_two_keys(KC_A, KC_S))
+//     {
+//         // Leader, a, s => GUI+S
+//         tap_code16(LGUI(KC_S));
+//     }
+// }
+
+
 
 
 void set_all_keys_in_layer(uint8_t layer, \
@@ -230,7 +451,7 @@ void do_mode_lighting(uint8_t led_min, uint8_t led_max)
     {
         goto exit;
     }
-    
+
     bool rgb_was_off = false;
     if (rgb_matrix_get_flags() == LED_FLAG_NONE)
     {
@@ -254,6 +475,12 @@ void do_mode_lighting(uint8_t led_min, uint8_t led_max)
     else if (rgb_was_off)
     {
         set_all_keys_in_layer(LAYER_VSCODE, led_min, led_max, RGB_OFF);
+    }
+
+    // turn del purple if alt-tab layer active
+    if (IS_LAYER_ON(LAYER_ENCODER_ALTTAB))
+    {
+        rgb_matrix_set_color(13, RGB_PURPLE);
     }
 
 exit:
@@ -328,12 +555,16 @@ bool dip_switch_update_user(uint8_t index, bool val)
             {
                 //SEND_STRING("WORK_MODE");
                 layer_on(BASE_LAYER_WORK);
+                layer_on(LAYER_GIT);
+                layer_on(LAYER_ENCODER_ALTTAB);
                 set_base_work_rgb();
             }
             else
             {
                 //SEND_STRING("HOME_MODE");
                 layer_off(BASE_LAYER_WORK);
+                layer_off(LAYER_GIT);
+                layer_off(LAYER_ENCODER_ALTTAB);
             }
         }
     }
